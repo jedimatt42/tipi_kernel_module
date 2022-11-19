9cf87ca3d6cc05df3cffcb477ec93fb521eb99d1
Matthew Splett <jedimatt42@protonmail.com>
2022-11-18 22:31:00 -0800

    force RPi pinmap

--- discover_pins.sh
+++ discover_pins.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-chips=`gpiodetect | cut -f1 -d' '`
+function outputRpiMap {
+  echo PIN_31=6
+  echo PIN_31_CHIP=gpiochip0
+  echo PIN_32=12
+  echo PIN_32_CHIP=gpiochip0
+  echo PIN_33=13
+  echo PIN_33_CHIP=gpiochip0
+  echo PIN_35=19
+  echo PIN_35_CHIP=gpiochip0
+  echo PIN_36=16
+  echo PIN_36_CHIP=gpiochip0
+  echo PIN_37=26
+  echo PIN_37_CHIP=gpiochip0
+  echo PIN_38=20
+  echo PIN_38_CHIP=gpiochip0
+  echo PIN_40=21
+  echo PIN_40_CHIP=gpiochip0
+}
 
-function searchPotatoPin {
+function searchLePotatoPin {
   CHIP=$1; shift 
   PIN=$1; shift
   NUM=`gpioinfo $CHIP | grep "\"7J1 Header Pin$PIN\"" | cut -f 1 -d':' | sed 's#^\s*line\s*##'`
@@ -12,11 +29,20 @@ function searchPotatoPin {
   fi
 }
 
+# enumerate each gpio chip and map available pins
+
+chips=`gpiodetect | cut -f1 -d' '`
+
 for chip in $chips; do
   gpioinfo $chip | grep '7J1 Header Pin' >/dev/null 2>&1
   if [ $? -eq 0 ]; then
-    for pin in 3 5 7 8 10 11 12 13 15 16 18 19 21 22 23 24 26 27 28 29 31 32 33 35 36 37 38 40; do
-      searchPotatoPin $chip $pin
+    for pin in 31 32 33 35 36 37 38 40; do
+      searchLePotatoPin $chip $pin
     done
+  else 
+    gpioinfo $chip | grep 'GPIO26' >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+      outputRpiMap
+    fi
   fi
 done
