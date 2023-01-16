df13e997dc3a4934247632dcb758a1267dafbea4
Matthew Splett <jedimatt42@protonmail.com>
2023-01-15 17:16:21 -0800

    set sig_delay to 100
    include setup of module param in install.sh

--- install.sh
+++ install.sh
@@ -9,4 +9,7 @@ mkdir -p /lib/modules/`uname -r`/kernel/drivers/tipi
 cp tipi_gpio.ko /lib/modules/`uname -r`/kernel/drivers/tipi/
 
 grep tipi_gpio /etc/modules >/dev/null || echo tipi_gpio >> /etc/modules
+
+echo "options tipi_gpio sig_delay=100" >/etc/modprobe.d/tipi_gpio.conf
+
 depmod
