8920ca0ed99cc0aa7cd256dfe14d7466ce77a417
Matthew Splett <jedimatt42@protonmail.com>
2023-01-21 15:17:52 -0800

    need to inform the PI kernel to load the overlay

--- install.sh
+++ install.sh
@@ -11,5 +11,7 @@ cp tipi_gpio.ko /lib/modules/`uname -r`/kernel/drivers/tipi/
 grep tipi_gpio /etc/modules >/dev/null || echo tipi_gpio >> /etc/modules
 
 echo "options tipi_gpio sig_delay=100" >/etc/modprobe.d/tipi_gpio.conf
+echo "# Enable TIPI overlay" >> /boot/config.txt
+echo "dtoverlay=tipi" >> /boot/config.txt
 
 depmod
