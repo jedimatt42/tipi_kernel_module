d324dbc5da8c7de78406c844e07fee681b598ed8
Matthew Splett <jedimatt42@protonmail.com>
2023-01-26 22:23:34 -0800

    won't rebuild if not clean

--- onboot.sh
+++ onboot.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [ ! -e /dev/tipi_control ]; then
-  cd /home/tipi/tipi_kernel_module.sh
+  cd /home/tipi/tipi_kernel_module
+  su tipi -c "make clean"
   su tipi -c make
   ./install.sh
   insmod /lib/modules/`uname -r`/kernel/drivers/tipi/tipi_gpio.ko
