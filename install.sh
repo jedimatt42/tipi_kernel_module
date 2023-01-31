93f39cf7e7fd2707e3868ea5bcba95c83b264e72
Matthew Splett <jedimatt42@protonmail.com>
2023-01-30 23:33:44 -0800

    group RPI specific parts together

--- install.sh
+++ install.sh
@@ -8,12 +8,12 @@ fi
 mkdir -p /lib/modules/`uname -r`/kernel/drivers/tipi
 cp tipi_gpio.ko /lib/modules/`uname -r`/kernel/drivers/tipi/
 
-# This location only works for Raspberry PI
-cp tipi_rpi.dtbo /boot/overlays/tipi.dtbo
-
 grep tipi_gpio /etc/modules >/dev/null || echo tipi_gpio >> /etc/modules
 
 echo "options tipi_gpio sig_delay=100" >/etc/modprobe.d/tipi_gpio.conf
+
+# This location only works for Raspberry PI
+cp tipi_rpi.dtbo /boot/overlays/tipi.dtbo
 echo "# Enable TIPI overlay" >> /boot/config.txt
 echo "dtoverlay=tipi" >> /boot/config.txt
 
