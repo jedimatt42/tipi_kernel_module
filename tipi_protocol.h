110c7cfeb3270e7c9b91de11bbff8df3809f4b85
Matthew Splett <jedimatt42@protonmail.com>
2025-02-28 17:07:17 -0800

    most significant bits before least when shifting data in and out

--- tipi_protocol.h
+++ tipi_protocol.h
@@ -85,10 +85,10 @@ static unsigned char readReg(int reg) {
   signalDelay();
 
   // read the data from the nibbles
-  value |= gpiod_get_value(PIN_NIB0);
-  value |= gpiod_get_value(PIN_NIB1) << 1;
-  value |= gpiod_get_value(PIN_NIB2) << 2;
-  value |= gpiod_get_value(PIN_NIB3) << 3;
+  value |= gpiod_get_value(PIN_NIB0) << 4;
+  value |= gpiod_get_value(PIN_NIB1) << 5;
+  value |= gpiod_get_value(PIN_NIB2) << 6;
+  value |= gpiod_get_value(PIN_NIB3) << 7;
 
   // now clock the high 4 bits into the PI
   gpiod_set_value(PIN_CLK, 1);
@@ -99,11 +99,11 @@ static unsigned char readReg(int reg) {
   signalDelay();
 
   // read the data from the nibbles
-  value |= gpiod_get_value(PIN_NIB0) << 4;
-  value |= gpiod_get_value(PIN_NIB1) << 5;
-  value |= gpiod_get_value(PIN_NIB2) << 6;
-  value |= gpiod_get_value(PIN_NIB3) << 7;
- 
+  value |= gpiod_get_value(PIN_NIB0);
+  value |= gpiod_get_value(PIN_NIB1) << 1;
+  value |= gpiod_get_value(PIN_NIB2) << 2;
+  value |= gpiod_get_value(PIN_NIB3) << 3;
+
   return value;
 }
 
@@ -113,10 +113,10 @@ static void writeReg(unsigned char value, int reg) {
 
   // we are already in output mode
   // so now set nib0-nib3 to low 4 bits of value
-  gpiod_set_value(PIN_NIB0, value & 0x01);
-  gpiod_set_value(PIN_NIB1, value & 0x02);
-  gpiod_set_value(PIN_NIB2, value & 0x04);
-  gpiod_set_value(PIN_NIB3, value & 0x08);
+  gpiod_set_value(PIN_NIB0, value & 0x10); 
+  gpiod_set_value(PIN_NIB1, value & 0x20);
+  gpiod_set_value(PIN_NIB2, value & 0x40);
+  gpiod_set_value(PIN_NIB3, value & 0x80);
   signalDelay();
 
   // now clk the data into the register
@@ -125,10 +125,10 @@ static void writeReg(unsigned char value, int reg) {
   gpiod_set_value(PIN_CLK, 0);
 
   // now set nib0-nib3 to high 4 bits of value
-  gpiod_set_value(PIN_NIB0, value & 0x10); 
-  gpiod_set_value(PIN_NIB1, value & 0x20);
-  gpiod_set_value(PIN_NIB2, value & 0x40);
-  gpiod_set_value(PIN_NIB3, value & 0x80);
+  gpiod_set_value(PIN_NIB0, value & 0x01);
+  gpiod_set_value(PIN_NIB1, value & 0x02);
+  gpiod_set_value(PIN_NIB2, value & 0x04);
+  gpiod_set_value(PIN_NIB3, value & 0x08);
   signalDelay();
 
   // now clk that data into the register
