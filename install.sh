f0308da259ad8bc13bb75176c88463c1ae9a16de
Matthew Splett <jedimatt42@protonmail.com>
2025-08-03 10:39:20 -0700

    uninstall notes

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ grep tipi_pibus /etc/modules >/dev/null || echo tipi_pibus >> /etc/modules
 echo "options tipi_pibus sig_delay=100" >/etc/modprobe.d/tipi_pibus.conf
 
 # This location only works for Raspberry PI
-cp tipi_pibus.dtbo /boot/overlays/tipi.dtbo
+cp tipi_pibus.dtbo /boot/overlays/tipi_pibus.dtbo
 
 grep dtoverlay=tipi /boot/firmware/config.txt >/dev/null || echo "dtoverlay=tipi" >> /boot/firmware/config.txt
 
@@ -28,3 +28,16 @@ if [ "${1:x}" = "/r" ]; then
   echo "Reloading tipi_pibus.ko"
   insmod /lib/modules/`uname -r`/kernel/drivers/tipi/tipi_pibus.ko
 fi
+
+# TO UNINSTALL
+# 
+# sudo systemctl stop tipi.service
+# sudo systemctl stop tipiwatchdog.service
+# sudo rmmod tipi_pibus
+# sudo rm /etc/modprobe.d/tipi_pibus.conf
+# sudo rm -r /lib/modules/`uname -r`/kernel/drivers/tipi
+# sudo rm /boot/overlays/tipi_pibus.dtbo
+# 
+# vi /etc/modules, and remove tipi_pibus
+# sudo 
+# 
